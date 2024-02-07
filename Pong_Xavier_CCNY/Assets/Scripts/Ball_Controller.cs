e1092226ccacdf9837c2cb1340fb86bcb4227d6a
xavirolon <152827615+xavirolon@users.noreply.github.com>
2024-02-07 18:12:22 -0500

    Made Ball Move

--- Pong_Xavier_CCNY/Assets/Scripts/Ball_Controller.cs
+++ Pong_Xavier_CCNY/Assets/Scripts/Ball_Controller.cs
@@ -4,10 +4,44 @@ using UnityEngine;
 
 public class Ball_Controller : MonoBehaviour
 {
+    // GLOBAL VARIABLES (Universal)
+// variable naming convention: xPositionTall
+// rigidbody2D gives physics components to objects (mass, gravity, force, etc)
+    public Rigidbody2D rbBall;
+    public float force = 200;
+
+    private float xDir;
+    private float yDir;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, World!");
+        // Debug.Log("Hello, World!");
+        Vector3 direction = new Vector3(0, 0, 0);
+
+        xDir = Random.Range(0, 2);  // (0, 2) returns 0 or 1 only
+        // Debug.Log("xDir = " + xDir);
+        if (xDir == 0)
+        {
+            direction.x = -1;   // moves left
+        }
+        else if (xDir == 1)
+        {
+            direction.x = 1;    // move right
+        }
+        
+        yDir = Random.Range(0, 2);
+        if (yDir == 0)
+        {
+            direction.y = -1;   // move down
+        }
+        else if (yDir == 1)
+        {
+            direction.y = 1;    // move up
+        }
+
+        // add force to start movement
+        rbBall.AddForce(direction * force);
     }
 
     // Update is called once per frame
