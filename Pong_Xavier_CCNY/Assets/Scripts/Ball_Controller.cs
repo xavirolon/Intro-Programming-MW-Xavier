d5373af92225f44614998c7f563bbcf65bc43f5d
xavirolon <152827615+xavirolon@users.noreply.github.com>
2024-02-14 18:05:15 -0500

    Ball Reset

--- Pong_Xavier_CCNY/Assets/Scripts/Ball_Controller.cs
+++ Pong_Xavier_CCNY/Assets/Scripts/Ball_Controller.cs
@@ -6,20 +6,44 @@ public class Ball_Controller : MonoBehaviour
 {
     // GLOBAL VARIABLES (Universal)
 // variable naming convention: xPositionTall
+
 // rigidbody2D gives physics components to objects (mass, gravity, force, etc)
     public Rigidbody2D rbBall;
-    public float force = 200;
+    public float force = 200; // declare and set force variable
 
-    private float xDir;
-    private float yDir;
+    private float xDir; // declare x direction float
+    private float yDir; // declare y direction float
+
+    public bool inPlay;
+    public Vector3 ballStartPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Debug.Log("Hello, World!");
+        // this is a comment
+        // Debug.Log("Hello, World!"); // print to console
+        
+        Launch();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inPlay == false)
+        {
+            transform.position = ballStartPos;
+            Launch();
+        }
+    }
+    
+
+    void Launch()
+    {
         Vector3 direction = new Vector3(0, 0, 0);
 
-        xDir = Random.Range(0, 2);  // (0, 2) returns 0 or 1 only
+        // Set direction.x (which way the ball moves horizontally)
+        xDir = Random.Range(0, 2);  // pick a random int between 0 and 1
         // Debug.Log("xDir = " + xDir);
         if (xDir == 0)
         {
@@ -41,12 +65,19 @@ public class Ball_Controller : MonoBehaviour
         }
 
         // add force to start movement
-        rbBall.AddForce(direction * force);
+        rbBall.AddForce(direction * force); // launch the ball
+        inPlay = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    // EVENTS UPON COLLISION
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        // Debug.Log("object that collided w/ Ball: " + collision.gameObject.name);
+        if (collision.gameObject.name == "Left Wall" || collision.gameObject.name == "Right Wall")
+        {
+            // Debug.Log("collided with Left/Right Wall");
+            rbBall.velocity = Vector3.zero;
+            inPlay = false;
+        }
     }
 }
